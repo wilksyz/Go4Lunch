b32b80c939ea7b5706b39898f6d364f0e6e42453
wilksyz <antoine-p@hotmail.com>
2018-11-05 12:29:35 +0100

    add authentification

--- app/src/main/java/com/antoine/go4lunch/MainActivity.java
+++ app/src/main/java/com/antoine/go4lunch/MainActivity.java
@@ -7,38 +7,66 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
+    private static final int RC_SIGN_IN = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.configureBottomNavigationView();
+        this.checkIfUserConnected();
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+    }
+
+    private void configureBottomNavigationView(){
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navigation_home:
+
+                        return true;
+                    case R.id.navigation_dashboard:
+
+                        return true;
+                    case R.id.navigation_notifications:
+
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    private void checkIfUserConnected() {
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() == null) {
+            // already signed in
+            startSignInActivity();
+        }
+    }
+
+    private void startSignInActivity(){
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(Arrays.asList(
+                                new AuthUI.IdpConfig.EmailBuilder().build(),
+                                new AuthUI.IdpConfig.GoogleBuilder().build()))
+                        .setTheme(R.style.LoginTheme)
+                        .setLogo(R.mipmap.ic_google)
+                        .build(),
+                RC_SIGN_IN);
     }
 
 }
